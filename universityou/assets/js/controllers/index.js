f0d275c09b877ff397f3b11dcbb4ce6ff44f85b8
James Harrington <indoisfun@gmail.com>
2014-03-29 19:24:16 -0500

    still working on index

--- universityou/assets/js/controllers/index.js
+++ universityou/assets/js/controllers/index.js
@@ -1,4 +1,7 @@
-uniApp.factory('IndexCtrl', ['$http', function ($http) {
-
-    };
-}]);
+uniApp.controller('IndexCtrl', ['SubjectFactory','$scope', function(subject_list, $scope) {
+    $scope.subjects= [];
+    subject_list.all(function (results) {
+      $scope.subjects = results;
+    })
+    }
+]);
